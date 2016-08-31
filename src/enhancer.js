45645ee66f50ac055dc3c75ac55aea40cfd5c6a6
Zalmoxisus <zalmoxisus@gmail.com>
2016-08-31 22:25:50 +0300

    Implement the enhancer and add tests

--- src/enhancer.js
+++ src/enhancer.js
@@ -1,10 +1,90 @@
-export function remotedevEnhancer(options) {
+import { stringify } from 'jsan';
+
+function sender(data, sendTo) {
+  try {
+    fetch(sendTo, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }).catch(function (err) {
+      console.warn(err);
+    });
+  } catch (err) {
+    console.warn(err);
+  }
+}
+
+function prepare(data, options, action) {
+  return {
+    type: options.type,
+    action,
+    payload: data && stringify(data),
+    preloadedState: options.preloadedState && stringify(options.preloadedState),
+    title: options.title,
+    description: options.description,
+    screenshot: options.screenshot,
+    version: options.version,
+    user: options.user
+  };
+}
+
+function add(data, options, state) {
+  if (options.maxAge < options.data.length) {
+    const sData = options.data.shift();
+    if (sData) {
+      if (options.withState) options.preloadedState = sData.state;
+      else options.preloadedState = options.states.shift();
+    }
+  }
+  if (!options.withState) options.states.push(state);
+  options.data.push(data);
+}
+
+function preSend(action, store, options) {
+  let data;
+  const { onlyState, withState } = options;
+  const state = store.getState();
+  if (onlyState) {
+    options.preloadedState = state;
+  } else if (withState) {
+    data = { state, action };
+  } else {
+    data = action;
+  }
+  if (options.every) {
+    options.sender(prepare(data, options), options.sendTo);
+  } else {
+    if (!onlyState) add(data, options, state);
+    if (options.sendOn && options.sendOn === action.type) {
+      options.sender(prepare(options.data, options, action.type), options.sendTo);
+    }
+  }
+}
+
+export default function remotedevEnhancer(options) {
+  if (!options || !options.sendTo && !options.sender) {
+    throw new Error('Provide at least `sendTo` or `sender` option for remotedev enhancer.');
+  }
+  if (!options.sender) options.sender = sender;
+  if (options.onlyState) {
+    if (!options.type) options.type = 'STATE';
+  } else if (options.every) {
+    if (!options.type) options.type = 'ACTION';
+  } else {
+    if (!options.maxAge) options.maxAge = 30;
+    if (!options.type) options.type = !options.withState ? 'ACTIONS' : 'STATES';
+    options.data = [];
+  }
+  if (!options.withState && options.maxAge) options.states = [];
+
   return (createStore) => (reducer, preloadedState, enhancer) => {
     const store = createStore(reducer, preloadedState, enhancer);
 
     const dispatch = (action) => {
       const r = store.dispatch(action);
-      console.log(action, store.getState());
+      preSend(action, store, options);
       return r;
     };
 
---
+++ test/enhancer.spec.js
@@ -0,0 +1,153 @@
+import expect, { spyOn } from 'expect';
+import { createStore } from 'redux';
+import remotedev from '../src/enhancer';
+
+function counter(state = 0, action) {
+  switch (action.type) {
+    case 'INCREMENT': return state + 1;
+    case 'DECREMENT': return state - 1;
+    default: return state;
+  }
+}
+
+describe('enhancer', () => {
+  it('throws if there are no arguments', () => {
+    expect(() => {
+      createStore(counter, remotedev());
+    }).toThrow(
+      'Provide at least `sendTo` or `sender` option for remotedev enhancer.'
+    );
+  });
+
+  it('should send every action (`every: true`)', () => {
+    const options = {
+      every: true,
+      sender: () => {}
+    };
+    const spy = spyOn(options, 'sender');
+    const store = createStore(counter, remotedev(options));
+    expect(spy.calls.length).toEqual(0);
+    expect(store.getState()).toBe(0);
+    store.dispatch({ type: 'INCREMENT' });
+    expect(store.getState()).toBe(1);
+    expect(spy.calls.length).toEqual(1);
+    expect(spy.calls[0].arguments[0].type).toBe('ACTION');
+    expect(spy.calls[0].arguments[0].payload).toBe('{"type":"INCREMENT"}');
+    store.dispatch({ type: 'INCREMENT' });
+    expect(store.getState()).toBe(2);
+    expect(spy.calls.length).toEqual(2);
+    expect(spy.calls[1].arguments[0].type).toBe('ACTION');
+    expect(spy.calls[1].arguments[0].payload).toBe('{"type":"INCREMENT"}');
+  });
+
+  it('should warn that fetch is not defined', () => {
+    const options = {
+      sendTo: '-',
+      every: true
+    };
+    const spy = spyOn(console, 'warn');
+    const store = createStore(counter, remotedev(options));
+    expect(spy.calls.length).toEqual(0);
+    expect(store.getState()).toBe(0);
+    store.dispatch({ type: 'INCREMENT' });
+    expect(store.getState()).toBe(1);
+    expect(spy.calls.length).toEqual(1);
+    expect(spy.calls[0].arguments[0].toString()).toBe('ReferenceError: fetch is not defined');
+  });
+
+  it('should send batched actions for `sendOn` action', () => {
+    const options = {
+      sendOn: 'DECREMENT',
+      sender: () => {}
+    };
+    const spy = spyOn(options, 'sender');
+    const store = createStore(counter, remotedev(options));
+    expect(spy.calls.length).toEqual(0);
+    expect(store.getState()).toBe(0);
+    store.dispatch({ type: 'INCREMENT' });
+    expect(store.getState()).toBe(1);
+    expect(spy.calls.length).toEqual(0);
+    store.dispatch({ type: 'INCREMENT' });
+    expect(store.getState()).toBe(2);
+    expect(spy.calls.length).toEqual(0);
+    store.dispatch({ type: 'DECREMENT' });
+    expect(store.getState()).toBe(1);
+    expect(spy.calls.length).toEqual(1);
+    expect(spy.calls[0].arguments[0].type).toBe('ACTIONS');
+    expect(spy.calls[0].arguments[0].payload).toBe(
+      '[{"type":"INCREMENT"},{"type":"INCREMENT"},{"type":"DECREMENT"}]'
+    );
+    expect(spy.calls[0].arguments[0].preloadedState).toBe(undefined);
+  });
+
+  it('should commit state for maxAge', () => {
+    const options = {
+      sendOn: 'DECREMENT',
+      maxAge: 1,
+      sender: () => {}
+    };
+    const spy = spyOn(options, 'sender');
+    const store = createStore(counter, remotedev(options));
+    expect(store.getState()).toBe(0);
+    store.dispatch({ type: 'INCREMENT' });
+    expect(store.getState()).toBe(1);
+    store.dispatch({ type: 'INCREMENT' });
+    expect(store.getState()).toBe(2);
+    expect(spy.calls.length).toEqual(0);
+    store.dispatch({ type: 'DECREMENT' });
+    expect(store.getState()).toBe(1);
+    expect(spy.calls.length).toEqual(1);
+    expect(spy.calls[0].arguments[0].type).toBe('ACTIONS');
+    expect(spy.calls[0].arguments[0].payload).toBe(
+      '[{"type":"INCREMENT"},{"type":"DECREMENT"}]'
+    );
+    expect(spy.calls[0].arguments[0].preloadedState).toBe('1');
+  });
+
+  it('should send actions and the state', () => {
+    const options = {
+      sendOn: 'DECREMENT',
+      withState: true,
+      sender: () => {}
+    };
+    const spy = spyOn(options, 'sender');
+    const store = createStore(counter, remotedev(options));
+    expect(store.getState()).toBe(0);
+    store.dispatch({ type: 'INCREMENT' });
+    expect(store.getState()).toBe(1);
+    store.dispatch({ type: 'INCREMENT' });
+    expect(store.getState()).toBe(2);
+    expect(spy.calls.length).toEqual(0);
+    store.dispatch({ type: 'DECREMENT' });
+    expect(store.getState()).toBe(1);
+    expect(spy.calls.length).toEqual(1);
+    expect(spy.calls[0].arguments[0].type).toBe('STATES');
+    expect(spy.calls[0].arguments[0].payload).toBe(
+      '[{"state":1,"action":{"type":"INCREMENT"}},' +
+      '{"state":2,"action":{"type":"INCREMENT"}},' +
+      '{"state":1,"action":{"type":"DECREMENT"}}]'
+    );
+  });
+
+  it('should send only the state', () => {
+    const options = {
+      sendOn: 'DECREMENT',
+      onlyState: true,
+      sender: () => {}
+    };
+    const spy = spyOn(options, 'sender');
+    const store = createStore(counter, remotedev(options));
+    expect(store.getState()).toBe(0);
+    store.dispatch({ type: 'INCREMENT' });
+    expect(store.getState()).toBe(1);
+    store.dispatch({ type: 'INCREMENT' });
+    expect(store.getState()).toBe(2);
+    expect(spy.calls.length).toEqual(0);
+    store.dispatch({ type: 'DECREMENT' });
+    expect(store.getState()).toBe(1);
+    expect(spy.calls.length).toEqual(1);
+    expect(spy.calls[0].arguments[0].type).toBe('STATE');
+    expect(spy.calls[0].arguments[0].preloadedState).toBe('1');
+    expect(spy.calls[0].arguments[0].payload).toBe(undefined);
+  });
+});
